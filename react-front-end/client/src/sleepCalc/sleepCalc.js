2468539a93f76d4fca9f3e2e4743382950c314ff
Gary Carmichael <g.carmichael90@gmail.com>
2016-03-07 10:00:25 +0000

    Updated tests. Now able to calculate four wake times.

--- react-front-end/client/src/sleepCalc/sleepCalc.js
+++ react-front-end/client/src/sleepCalc/sleepCalc.js
@@ -1,5 +1,34 @@
 var moment = require('moment');
 
 var SleepCalc = function(){
-
+  this.wakeTime = undefined;
+  this.sleepTime = undefined;
+  this.cycleDuration = moment.duration(1.5, 'hours').asHours();
 };
+
+SleepCalc.prototype = {
+
+  addHours: function(time){
+    var newTime = moment(time, 'H:mm').add(this.cycleDuration, 'hours').format('H:mm');
+    return newTime;
+  },
+
+  calcWakeTime: function(){
+    this.sleepTime = moment().format('H:mm'); // Add 14 minutes
+    var wake1 = this.addHours(this.sleepTime);
+    var wake2 = this.addHours(wake1);
+    var wake3 = this.addHours(wake2);
+    var wake4 = this.addHours(wake3);
+    var wake5 = this.addHours(wake4);
+
+    return {
+      wake1: wake1,
+      wake2: wake2,
+      wake3: wake3,
+      wake4: wake4,
+      wake5: wake5
+    }
+  }
+};
+
+module.exports = SleepCalc;
--- react-front-end/client/src/sleepCalc/specs/sleepCalc_spec.js
+++ react-front-end/client/src/sleepCalc/specs/sleepCalc_spec.js
@@ -2,45 +2,50 @@ var SleepCalc = require('../sleepCalc');
 var assert = require('assert');
 var moment = require('moment');
 
-beforeEach(function() {
-    var sleepCalc = new SleepCalc();
-  });
-
 describe('SleepCalc', function(){
+
   it('should start with undefined wake and sleep times', function(){
+    var sleepCalc = new SleepCalc();
     assert.deepEqual(undefined, sleepCalc.wakeTime);
     assert.deepEqual(undefined, sleepCalc.sleepTime);
   });
 
   it('should be able to take moments as values', function(){
-    assert(false, sleepCalc.wakeTime);
-    sleepCalc.wakeTime = moment();
-    assert(true, sleepCalc.wakeTime);
+    var sleepCalc = new SleepCalc();
+    assert.deepEqual(undefined, sleepCalc.wakeTime);
+    sleepCalc.wakeTime = moment().format('H:mm');
+    assert.equal(moment().format('H:mm'), sleepCalc.wakeTime);
   });
 
   it('should have a duration variable with a value of 1.5 hours', function(){
-    assert.deepEqual("1.5 hours", sleepCalc.cycleDuration);
+    var sleepCalc = new SleepCalc();
+    assert.deepEqual(1.5, sleepCalc.cycleDuration);
   });
 
   it('should be able to set a sleepTime through calcWakeTime', function(){
+    var sleepCalc = new SleepCalc();
     sleepCalc.calcWakeTime();
-    assert.deepEqual(true, sleepCalc.sleepTime);
+    assert.deepEqual(moment().format('H:mm'), sleepCalc.sleepTime);
   });
 
   it('should be able to calculate four wake times', function(){
-    sleepCalc.calcWakeTime();
-    assert.equal((sleepCalc.sleepTime + sleepCalc.cycleDuration), sleepCalc.wake1);
-    assert.equal((sleepCalc.wake1 + sleepCalc.cycleDuration), sleepCalc.wake2);
-    assert.equal((sleepCalc.wake2 + sleepCalc.cycleDuration), sleepCalc.wake3);
-    assert.equal((sleepCalc.wake3 + sleepCalc.cycleDuration), sleepCalc.wake4);
+    var sleepCalc = new SleepCalc();
+    var wakeTimes = sleepCalc.calcWakeTime();
+    console.log(wakeTimes.wake1);
+    assert.equal(moment().add(1.5, 'hours').format('H:mm'), wakeTimes.wake1);
+    assert.equal(moment(wakeTimes.wake1, 'H:mm').add(sleepCalc.cycleDuration, 'hours').format('H:mm'), wakeTimes.wake2);
+    assert.equal(moment(wakeTimes.wake2, 'H:mm').add(sleepCalc.cycleDuration, 'hours').format('H:mm'), wakeTimes.wake3);
+    assert.equal(moment(wakeTimes.wake3, 'H:mm').add(sleepCalc.cycleDuration, 'hours').format('H:mm'), wakeTimes.wake4);
   });
 
   it('should be able to set a wakeTime through calcSleepTime', function(){
+    var sleepCalc = new SleepCalc();
     sleepCalc.calcSleepTime();
     assert.deepEqual(true, sleepCalc.wakeTime);
   });
 
   it('should be able to calculate four sleep times', function(){
+    var sleepCalc = new SleepCalc();
     sleepCalc.calcSleepTime();
     assert.equal((sleepCalc.wakeTime - sleepCalc.cycleDuration), sleepCalc.sleep1);
     assert.equal((sleepCalc.sleep1 - sleepCalc.cycleDuration), sleepCalc.sleep2);
